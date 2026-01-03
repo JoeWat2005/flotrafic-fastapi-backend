c50213c744fb5166a949153f3c2fd0fdf4c43004
Joseph Watmough <watmoughjoe2005@gmail.com>
2026-01-03 14:24:31 +0000

    change to postgres

--- app/db/session.py
+++ app/db/session.py
@@ -1,16 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "sqlite:///./dev.db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql+psycopg2://flotrafic:flotrafic@db:5432/flotrafic"
+)
 
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL,
+    pool_pre_ping=True,
 )
 
 SessionLocal = sessionmaker(
     bind=engine,
     autocommit=False,
-    autoflush=False
+    autoflush=False,
 )
 
 def get_db():
